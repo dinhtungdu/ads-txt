efbe60da94d1ea8b0fe2cf4e991c831c05d733b8
Tung Du <dinhtungdu@gmail.com>
2022-02-16 16:04:03 +0700

    hehe

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -2,7 +2,7 @@ describe("Admin can login and make sure plugin is activated", () => {
   it("Permalink structure works", () => {
     cy.visitAdminPage('options-general.php?page=wp-htaccess-editor');
     cy.get('.toggle-card').click();
-    cy.get('.wphe-dismiss-notice').click();
+    cy.get('.wphe-dismiss-notice:visible').first().click();
     cy.screenshot();
     cy.visit("/hello-world");
   });
