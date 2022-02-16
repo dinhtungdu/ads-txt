03baf463d31935aebaeb7df4375aea4cde3a02cb
Tung Du <dinhtungdu@gmail.com>
2022-02-16 15:46:38 +0700

    oh yeah

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -1,17 +1,9 @@
 describe("Admin can login and make sure plugin is activated", () => {
   it("Permalink structure works", () => {
-    cy.visitAdminPage( `options-permalink.php` );
-    cy.get( 'h1' ).should("have.text", "Permalink Settings");
-    cy.get( '#permalink_structure' ).should("have.value", "/%postname%/");
+    cy.visitAdminPage('options-general.php?page=wp-htaccess-editor');
+    cy.get('.toggle-card').click();
+    cy.get('.wphe-dismiss-notice').click();
+    cy.screenshot();
     cy.visit("/hello-world");
-  });
-
-  it("Can deactivate plugin and activate it back", () => {
-    cy.visitAdminPage("plugins.php");
-    cy.get("#deactivate-ads-txt, tr[data-slug=ads-txt] .deactivate a").click();
-    cy.get("#activate-ads-txt, tr[data-slug=ads-txt] .activate a").click();
-    cy.get("#deactivate-ads-txt, tr[data-slug=ads-txt] .deactivate a").should(
-      "be.visible"
-    );
   });
 });
